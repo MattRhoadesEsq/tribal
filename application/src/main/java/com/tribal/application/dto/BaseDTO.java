45c2a18628ca73875cf7db31c82caee1cdc24511
MattRhoadesEsq <matt.rhoades.esq@gmail.com>
2018-11-08 11:51:50 -0800

    Add httpClient/restClient Add general use HarnessException Add testing TAssert (Tribal Assert) Implement basic "GET" test Implement basic "POST" test

--- application/src/main/java/com/tribal/application/dto/BaseDTO.java
+++ application/src/main/java/com/tribal/application/dto/BaseDTO.java
@@ -1,11 +1,13 @@
 package com.tribal.application.dto;
 
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.json.JSONObject;
 
 
+@JsonTypeInfo(use=JsonTypeInfo.Id.CLASS, include=JsonTypeInfo.As.PROPERTY, property="@class")
 public class BaseDTO extends JSONObject {
     protected static Logger LOG = LogManager.getLogger(BaseDTO.class);
 }
